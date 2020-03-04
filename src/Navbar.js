4d4d973a3738ef004473ed047591ffa89fbd27df
Lior Rotberg <Liorr3@mail.afeka.ac.il>
2020-03-04 20:45:19 +0200

    Add Navbar and Footer to SingleColorPalette

--- src/Navbar.js
+++ src/Navbar.js
@@ -24,40 +24,42 @@ export default class Navbar extends Component {
     this.setState({ open: false });
   }
   render() {
-    const { level, changeLevel } = this.props;
+    const { level, changeLevel, showingAllColors } = this.props;
     const { format, open } = this.state;
     return (
       <header className="Navbar">
         <div className="logo">
           <Link to="/">reactcolorpicker</Link>
         </div>
-        <div className="slider-container">
-          <span>level:{level}</span>
-          <div className="slider">
-            <Slider
-              defaultValue={level}
-              min={100}
-              max={900}
-              step={100}
-              onAfterChange={changeLevel}
-              trackStyle={[{ backgroundColor: "transparent" }]}
-              handleStyle={[
-                {
+        {showingAllColors && (
+          <div className="slider-container">
+            <span>level:{level}</span>
+            <div className="slider">
+              <Slider
+                defaultValue={level}
+                min={100}
+                max={900}
+                step={100}
+                onAfterChange={changeLevel}
+                trackStyle={[{ backgroundColor: "transparent" }]}
+                handleStyle={[
+                  {
+                    backgroundColor: "green",
+                    outline: "none",
+                    border: "2px solid green",
+                    boxShadow: "none"
+                  }
+                ]}
+                activeDotStyle={{
                   backgroundColor: "green",
                   outline: "none",
                   border: "2px solid green",
                   boxShadow: "none"
-                }
-              ]}
-              activeDotStyle={{
-                backgroundColor: "green",
-                outline: "none",
-                border: "2px solid green",
-                boxShadow: "none"
-              }}
-            />
+                }}
+              />
+            </div>
           </div>
-        </div>
+        )}
         <div className="select-container">
           <Select value={format} onChange={this.handleFormatChange}>
             <MenuItem value="hex">HEX - </MenuItem>
--- src/Palette.js
+++ src/Palette.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import ColorBox from "./ColorBox";
 import Navbar from "./Navbar";
+import PaletteFooter from "./PaletteFooter";
 import "./Palette.css";
 
 export default class Palette extends Component {
@@ -34,12 +35,10 @@ export default class Palette extends Component {
           level={level}
           changeLevel={this.changeLevel}
           handleChange={this.changeFormat}
+          showingAllColors
         />
         <div className="Palette-colors">{ColorBoxes}</div>
-        <footer className="Palette-footer">
-          {paletteName}
-          <span className="emoji">{emoji}</span>
-        </footer>
+        <PaletteFooter paletteName={paletteName} emoji={emoji} />
       </div>
     );
   }
