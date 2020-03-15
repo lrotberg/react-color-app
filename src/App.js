8a56f3933f571fff50580e55fe76710eb4f17333
Lior Rotberg <Liorr3@mail.afeka.ac.il>
2020-03-15 16:52:09 +0200

    Finish MiniPalette delete

--- src/App.js
+++ src/App.js
@@ -14,11 +14,18 @@ class App extends Component {
     this.state = { palettes: savedPalettes || seedColors };
     this.findPalette = this.findPalette.bind(this);
     this.savePalette = this.savePalette.bind(this);
+    this.deletePalette = this.deletePalette.bind(this);
   }
   findPalette(id) {
     return this.state.palettes.find(function(palette) {
       return palette.id === id;
     });
+  }
+  deletePalette(id) {
+    this.setState(
+      st => ({ palettes: st.palettes.filter(palette => palette.id !== id) }),
+      this.syncLocalStorage
+    );
   }
   savePalette(newPalette) {
     this.setState(
@@ -62,7 +69,11 @@ class App extends Component {
           exact
           path="/"
           render={routeProps => (
-            <PaletteList palettes={this.state.palettes} {...routeProps} />
+            <PaletteList
+              palettes={this.state.palettes}
+              {...routeProps}
+              deletePalette={this.deletePalette}
+            />
           )}
         />
         <Route
@@ -77,9 +88,6 @@ class App extends Component {
           )}
         />
       </Switch>
-      // <div>
-      //   <Palette palette={generatePalette(seedColors[4])} />
-      // </div>
     );
   }
 }
