522aed52567596efa125daab95c98cda5373cc72
Lior Rotberg <Liorr3@mail.afeka.ac.il>
2020-03-19 20:43:40 +0200

    Clean up route animations

--- src/styles/PaletteStyles.js
+++ src/styles/PaletteStyles.js
@@ -1,21 +1,5 @@
 import sizes from "./sizes";
 const styles = {
-  "@global": {
-    ".fade-enter": {
-      opacity: 0
-    },
-    ".fade-enter-active": {
-      opacity: 1,
-      transition: "opacity 500ms ease-in-out"
-    },
-    ".fade-exit": {
-      opacity: 1
-    },
-    ".fade-exit-active": {
-      opacity: 0,
-      transition: "opacity 500ms ease-out"
-    }
-  },
   Palette: {
     height: "100vh",
     display: "flex",
