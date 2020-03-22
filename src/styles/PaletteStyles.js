b87a35a8672b40bd727275c825dad5ecfdaf0bd7
Lior Rotberg <Liorr3@mail.afeka.ac.il>
2020-03-22 16:20:19 +0200

    SinglePalette goBack button margin fix

--- src/styles/PaletteStyles.js
+++ src/styles/PaletteStyles.js
@@ -15,7 +15,7 @@ const styles = {
     display: "inline-block",
     position: "relative",
     cursor: "pointer",
-    marginBottom: "-3.5px",
+    marginBottom: "-5.5px",
     opacity: "1",
     backgroundColor: "black",
     "& a": {
