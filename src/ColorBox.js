8a5c1f782f03a521c460433244d69211e5c9c1d9
Lior Rotberg <Exolite@gmail.com>
2021-07-03 03:50:07 +0300

    Format ColorBox to be a functional component

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -1,61 +1,53 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import { CopyToClipboard } from "react-copy-to-clipboard";
 import { Link } from "react-router-dom";
 import { withStyles } from "@material-ui/styles";
 import classNames from "classnames";
 import styles from "./styles/ColorBoxStyles";
 
-class ColorBox extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { copied: false };
-    this.changeCopyState = this.changeCopyState.bind(this);
-  }
-  changeCopyState() {
-    this.setState({ copied: true }, () => {
-      setTimeout(() => this.setState({ copied: false }), 1500);
-    });
-  }
-  render() {
-    const {
-      name,
-      background,
-      moreUrl,
-      showingFullPalette,
-      classes
-    } = this.props;
-    const { copied } = this.state;
-    return (
-      <CopyToClipboard text={background} onCopy={this.changeCopyState}>
-        <div style={{ background }} className={classes.colorBox}>
-          <div
-            style={{ background }}
-            className={classNames(classes.copyOverlay, {
-              [classes.showOverlay]: copied
-            })}
-          />
-          <div
-            className={classNames(classes.copyMsg, {
-              [classes.showMsg]: copied
-            })}
-          >
-            <h1>copied!</h1>
-            <p className={classes.copyText}>{background}</p>
-          </div>
-          <div>
-            <div className={classes.boxContent}>
-              <span className={classes.colorName}>{name}</span>
-            </div>
-            <button className={classes.copyButton}>Copy</button>
-          </div>
-          {showingFullPalette && (
-            <Link to={moreUrl} onClick={e => e.stopPropagation()}>
-              <span className={classes.seeMore}>MORE</span>
-            </Link>
-          )}
+const ColorBox = props => {
+  const { name, background, moreUrl, showingFullPalette, classes } = props;
+  const [copied, setCopied] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => setCopied(false), 1500);
+  }, [copied]);
+
+  const changeCopyState = () => {
+    setCopied(true);
+  };
+
+  return (
+    <CopyToClipboard text={background} onCopy={changeCopyState}>
+      <div style={{ background }} className={classes.colorBox}>
+        <div
+          style={{ background }}
+          className={classNames(classes.copyOverlay, {
+            [classes.showOverlay]: copied
+          })}
+        />
+        <div
+          className={classNames(classes.copyMsg, {
+            [classes.showMsg]: copied
+          })}
+        >
+          <h1>copied!</h1>
+          <p className={classes.copyText}>{background}</p>
         </div>
-      </CopyToClipboard>
-    );
-  }
-}
+        <div>
+          <div className={classes.boxContent}>
+            <span className={classes.colorName}>{name}</span>
+          </div>
+          <button className={classes.copyButton}>Copy</button>
+        </div>
+        {showingFullPalette && (
+          <Link to={moreUrl} onClick={e => e.stopPropagation()}>
+            <span className={classes.seeMore}>MORE</span>
+          </Link>
+        )}
+      </div>
+    </CopyToClipboard>
+  );
+};
+
 export default withStyles(styles)(ColorBox);
